ea072de83c12975726fc528688c09e3ecbcbce81
JSP_SY <javasyp@gmail.com>
2023-04-06 18:02:40 +0900

    구구단 기초, 파라미터 X

---
+++ src/main/java/com/KoreaIT/java/jam/HomeMainServlet3.java
@@ -0,0 +1,23 @@
+package com.KoreaIT.java.jam;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/printDan")
+public class HomeMainServlet3 extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+
+		response.getWriter().append("8단<br>");
+
+		int dan = 8;
+		for (int i = 1; i <= 9; i++) {
+			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan * i));
+		}
+	}
+}
