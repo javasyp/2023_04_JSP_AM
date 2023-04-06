1555b11cdf1fddd9336ee0e8db24d60eba73da91
JSP_SY <javasyp@gmail.com>
2023-04-06 18:44:43 +0900

    파라미터에 따른 텍스트의 색상 변경

--- src/main/java/com/KoreaIT/java/jam/HomeMainServlet3.java
+++ src/main/java/com/KoreaIT/java/jam/HomeMainServlet3.java
@@ -16,6 +16,7 @@ public class HomeMainServlet3 extends HttpServlet {
 		// dan 이라는 파라미터 값 가져오기
 		String inputedDan = request.getParameter("dan");
 		String inputedLimit = request.getParameter("limit");
+		String inputedColor = request.getParameter("color");
 		
 		// 파라미터 없을 시, 기본값 넣어주기
 		if (inputedDan == null) {
@@ -26,15 +27,19 @@ public class HomeMainServlet3 extends HttpServlet {
 			inputedLimit = "1";
 		}
 		
+		if (inputedColor == null) {
+			inputedColor = "black";
+		}
+		
 		// 파라미터(문자열) 값을 숫자로 변환
 		int dan = Integer.parseInt(inputedDan);
 		int limit = Integer.parseInt(inputedLimit);
 		
 		// text/html 형식이므로 포맷팅 해주기
-		response.getWriter().append(String.format("==%d단==<br>", dan));
+		response.getWriter().append(String.format("<div style='color: %s;'>==%d단==</div>", inputedColor, dan));
 		
 		for (int i = 1; i <= limit; i++) {
-			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan * i));
+			response.getWriter().append(String.format("<div style='color: %s;'>%d * %d = %d</div>", inputedColor, dan, i, dan * i));
 		}
 	}
 }
