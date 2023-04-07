a7850a96bf98e993e728a41bf81b791e56f99b6b
JSP_SY <javasyp@gmail.com>
2023-04-07 15:27:49 +0900

    게시물 상세페이지 구현

---
+++ src/main/java/com/KoreaIT/java/jam/ArticleDetailServlet.java
@@ -0,0 +1,73 @@
+package com.KoreaIT.java.jam;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.KoreaIT.java.jam.util.DBUtil;
+
+@WebServlet("/article/detail")
+public class ArticleDetailServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html; charset=UTF-8");
+
+		// DB 연결
+		Connection conn = null;
+		
+		String url = "jdbc:mysql://127.0.0.1:3306/JAM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+		String user = "root";
+		String password = "";
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("예외) 클래스가 없습니다! 프로그램 종료");
+			e.printStackTrace();
+			return;
+		}
+
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+
+			response.getWriter().append("Success!!!");
+
+			DBUtil dbUtil = new DBUtil(request, response);
+			
+			// 파라미터 값 받아오기
+			int id = Integer.parseInt(request.getParameter("id"));
+
+			String sql = String.format("SELECT * FROM article WHERE id = %d;", id);
+
+			Map<String, Object> articleRow = dbUtil.selectRow(conn, sql);
+
+			response.getWriter().append(articleRow.toString());
+			
+			request.setAttribute("articleRow", articleRow);
+			
+			request.getRequestDispatcher("/jsp/article/detail.jsp").forward(request, response);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
--- src/main/webapp/jsp/article/list.jsp
+++ src/main/webapp/jsp/article/list.jsp
@@ -12,53 +12,17 @@ List<Map<String, Object>> articleRows = (List<Map<String, Object>>) request.getA
 <title>게시물 리스트</title>
 </head>
 <body>
-	<h1>게시물 리스트 v1</h1>
-
-	<ul>
-		<li><%=articleRows.get(0).get("id")%>번, <%=articleRows.get(0).get("regDate")%>,
-			<%=articleRows.get(0).get("title")%>, <%=articleRows.get(0).get("body")%></li>
-		<li><%=articleRows.get(1).get("id")%>번, <%=articleRows.get(1).get("regDate")%>,
-			<%=articleRows.get(1).get("title")%>, <%=articleRows.get(1).get("body")%></li>
-		<li><%=articleRows.get(2).get("id")%>번, <%=articleRows.get(2).get("regDate")%>,
-			<%=articleRows.get(2).get("title")%>, <%=articleRows.get(2).get("body")%></li>
-		<li><%=articleRows.get(3).get("id")%>번, <%=articleRows.get(3).get("regDate")%>,
-			<%=articleRows.get(3).get("title")%>, <%=articleRows.get(3).get("body")%></li>
-	</ul>
+	<h1>게시물 리스트</h1>
 	
-	<h1>게시물 리스트 v2</h1>
-
 	<ul>
 		<%
-		for (int i = 0; i <= 3; i++) {
+		for (Map<String, Object> articleRow : articleRows) {
 		%>
-		<li><%=articleRows.get(i).get("id")%>번, <%=articleRows.get(i).get("regDate")%>,
-			<%=articleRows.get(i).get("title")%>, <%=articleRows.get(i).get("body")%></li>
-		<%
-		}
-		%>
-	</ul>
-
-	<h1>게시물 리스트 v3</h1>
-
-	<ul>
-		<%
-		for (int i = 0; i < articleRows.size(); i++) {
-		%>
-		<li><%=articleRows.get(i).get("id")%>번, <%=articleRows.get(i).get("regDate")%>,
-			<%=articleRows.get(i).get("title")%>, <%=articleRows.get(i).get("body")%></li>
-		<%
-		}
-		%>
-	</ul>
-
-	<h1>게시물 리스트 v4</h1>
-
-	<ul>
-		<%
-		for (Map<String,Object> articleRow : articleRows) {
-		%>
-		<li><%=articleRow.get("id")%>번, <%=articleRow.get("regDate")%>,
-			<%=articleRow.get("title")%>, <%=articleRow.get("body")%></li>
+		<li><a href="detail?id=<%=articleRow.get("id")%>">
+				<%=articleRow.get("id")%>번  |  
+				<%=articleRow.get("regDate")%>  |  
+				<%=articleRow.get("title")%>  |  
+				<%=articleRow.get("body")%></a></li>
 		<%
 		}
 		%>
